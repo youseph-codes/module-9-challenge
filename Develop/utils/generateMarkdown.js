15d6e05bff1971c4e73fa8a5e517756d533a96d4
Yousopp <yousephagueye@gmail.com>
2022-10-08 11:58:51 -0400

    created function that returns license section of README.md

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -39,7 +39,17 @@ function renderLicenseLink(data) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+  const licenseInput = data.license
+
+  if (licenseInput === "none") {
+    return ""
+  } else {
+    return `# License
+    This application is under the following license:
+    ${renderLicenseBadge(data)}`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
