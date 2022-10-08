d8579c8a2b501c839fbb6d940960cebe8cf78e04
Yousopp <yousephagueye@gmail.com>
2022-10-08 12:18:46 -0400

    created packages and set up questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,60 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs")
+const inquirer = require("inquirer")
+const markdownGenerator = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
