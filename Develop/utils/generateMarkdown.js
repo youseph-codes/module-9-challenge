2629fb8d0441fa713d21892408d1b83d271b2aea
Yousopp <yousephagueye@gmail.com>
2022-10-08 11:28:17 -0400

    created license badge return function

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,44 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+function renderLicenseBadge(data) {
+  const licenseInput = data.license
+  let licenseBadge = ""
+  if (licenseInput === "none"){
+    return licenseBadge;
+  }
+  if (licenseInput === "MIT"){
+    licenseBadge = "[![License](/)](/)";
+    return licenseBadge;
+  }
+  if (licenseInput === "Apache"){
+    licenseBadge = "[![License](/)](/)";
+    return licenseBadge;
+  }
+  if (licenseInput === "BDS 3-Clause"){
+    licenseBadge = "[![License](/)](/)";
+    return licenseBadge;
+  }
+  if (licenseInput === "Mozilla"){
+    licenseBadge = "[![License](/)](/)";
+    return licenseBadge;
+  }
+}
+
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
