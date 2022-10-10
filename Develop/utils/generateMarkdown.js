d000c34b8e03d38d8b0651179dc8dc9bb5749c9e
Yousopp <yousephagueye@gmail.com>
2022-10-10 11:25:38 -0400

    reinstalled inquirer + added license badge on generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,19 +8,19 @@ function renderLicenseBadge(data) {
     return licenseBadge;
   }
   if (licenseInput === "MIT") {
-    licenseBadge = "[![License](/)](/)";
+    licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
     return licenseBadge;
   }
   if (licenseInput === "Apache") {
-    licenseBadge = "[![License](/)](/)";
+    licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
     return licenseBadge;
   }
   if (licenseInput === "BDS 3-Clause") {
-    licenseBadge = "[![License](/)](/)";
+    licenseBadge = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
     return licenseBadge;
   }
   if (licenseInput === "Mozilla") {
-    licenseBadge = "[![License](/)](/)";
+    licenseBadge = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
     return licenseBadge;
   }
 }
